a546e5c3597800e6bb18186e4189ded076cd8c15
veronica-7 <vettonica18@gmail.com>
2023-05-18 00:23:43 -0500

    Trabajo en Python

--- ejemplos/mis_clases.py
+++ ejemplos/mis_clases.py
@@ -7,7 +7,42 @@
 # Puede usar las mismas clases usadas en Java en los ejemplos estudiados
 
 # clase 01
+class Mascota:
+    humano = ''
+    tipo = ''
+    nombre = ''
+    color = ''
+    edad = 0
 
+    def __init__ (self, human, tip, nom, clr, edd):
+        self.humano = human
+        self.tipo = tip
+        self.nombre = nom
+        self.color = clr
+        self.edad = edd
+
+        def __str__ (self):
+            cadena = "Mi nombre es %s y tengo una mascota, su nombre es %s.\nEs un %s de color %s y tiene %d años de edad" .format(
+                self.humano, self.nombre, self.tipo, self.color, self.edad)
+            return cadena
 
 
 # clase 02
+class Bicicletas:
+    tipo = ''
+    color = ''
+    tamanio = ''
+    ruedas = 0
+    
+    def __init__(self, tip, clr, tam, rue):
+        self.tipo = tip
+        self.color = clr
+        self.tamanio = tam
+        self.ruedas = rue
+
+        def __str__ (self):
+            cadena = "Tengo un bicicleta %s, es de color %s, es %s y tiene %d.\n Es mi medio de transporte!" .format(
+                self.tipo, self.color, self.tamanio, self.ruedas)
+            return cadena
+
+
--- ejemplos/run.py
+++ ejemplos/run.py
@@ -2,17 +2,31 @@
 
 """
 # Crear dos objetos de la clase 01
+from mis_clases import Mascota
 
 # objeto 01
 
 # crear
+mascota1 = Mascota('Juan', 'gato', 'copito', 'blanco', 2)
 
 # Presentar objeto; usar el método __st__
+print(str(mascota1))
 
 # objeto 02
 
 # crear ingresando valores por teclado
+print("nombre del dueño:")
+humano = input()
+print("Ingrese el nombre de su mascota")
+nombre = input()
+print("Idique su tipo de mascota:")
+tipo = input()
+print("Indique el color de su mascota:")
+color = input()
+print("Indique la edad que tiene su mascota")
+edad = int(input())
+
+mascota2 = Mascota(humano, nombre, tipo, color, edad)
 
 # Presentar objeto; usar el método __st__
-
-
+print(str(mascota2))
