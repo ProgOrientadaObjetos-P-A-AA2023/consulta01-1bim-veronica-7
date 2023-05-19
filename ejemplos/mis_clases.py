da90d408a93afbb68d25292983920a9cfb5c684e
veronica-7 <vettonica18@gmail.com>
2023-05-18 23:24:34 -0500

    Trabajo en Python

--- ejemplos/mis_clases.py
+++ ejemplos/mis_clases.py
@@ -14,17 +14,18 @@ class Mascota:
     color = ''
     edad = 0
 
-    def __init__ (self, human, tip, nom, clr, edd):
+    def __init__(self, human, tip, nom, clr, edd):
         self.humano = human
-        self.tipo = tip
         self.nombre = nom
+        self.tipo = tip
         self.color = clr
         self.edad = edd
 
-        def __str__ (self):
-            cadena = "Mi nombre es %s y tengo una mascota, su nombre es %s.\nEs un %s de color %s y tiene %d años de edad" .format(
-                self.humano, self.nombre, self.tipo, self.color, self.edad)
-            return cadena
+
+    def __str__(self):
+        cadena = "Mi nombre es {} y tengo una mascota, su nombre es {} \nEs un{} de color {} y tiene {} años de edad".format(
+            self.humano, self.nombre, self.tipo, self.color, self.edad)
+        return cadena
 
 
 # clase 02
@@ -33,16 +34,16 @@ class Bicicletas:
     color = ''
     tamanio = ''
     ruedas = 0
-    
+
     def __init__(self, tip, clr, tam, rue):
         self.tipo = tip
         self.color = clr
         self.tamanio = tam
         self.ruedas = rue
 
-        def __str__ (self):
-            cadena = "Tengo un bicicleta %s, es de color %s, es %s y tiene %d.\n Es mi medio de transporte!" .format(
-                self.tipo, self.color, self.tamanio, self.ruedas)
-            return cadena
+    def __str__(self):
+        cadena = "Tengo una bicicleta {}, es de color {}, es {} y tiene {} \nEs mi medio de transporte!".format(
+            self.tipo, self.color, self.tamanio, self.ruedas)
+        return cadena
 
 
--- ejemplos/run2.py
+++ ejemplos/run2.py
@@ -2,17 +2,29 @@
 
 """
 # Crear dos objetos de la clase 02
+from mis_clases import Bicicletas
 
 # objeto 01
 
 # crear
+bicicleta1 = Bicicletas('BMX', 'rojo', 'pequeña', 2)
 
 # Presentar objeto; usar el método __st__
+print(bicicleta1.__str__())
 
 # objeto 02
 
 # crear ingresando valores por teclado
+print("Tipo de Bicicleta:")
+tipo = input()
+print("Idique el color de la bicicleta")
+color = input()
+print("Tamaño de la bicicleta:")
+tamanio = input()
+print("Cuántas ruedas tiene su bicicleta?")
+ruedas = int(input())
+
+bicicleta2 = Bicicletas(tipo, color, tamanio, ruedas)
 
 # Presentar objeto; usar el método __st__
-
-
+print(bicicleta2.__str__())
